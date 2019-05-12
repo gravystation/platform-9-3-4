aa65bb2ba6f5cf67b15bb2b5fdedac48340c0609
eric bistow <Conrad.Gessner@protonmail.com>
2019-05-12 16:28:14 +0100

    gen hex update

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -66,7 +66,7 @@ gravyd --print-genesis-tx --genesis-block-reward-address cubev2Fyavy8CXG8BPEbNeC
 * You should see your premine appear in the previously generated wallet.
 
 */
-const char GENESIS_COINBASE_TX_HEX[] = "01a82801ff0001ffbfa89cf99eadff3a02b728e90c06b3b9b68ad00d58fe4d367375322546e40355c52ab3d388783af831210130bee519a3ca65cf9461e7e876139547d3ea937a01d24cf7a95f0286e4cddae2";
+const char GENESIS_COINBASE_TX_HEX[] = "012801ff0001ffbfa89cf99eadff3a029e522e61d184e04f6de5c8acdb325de3c03cd278f350e9c95b068d5990648f1421010e09f39dedef400a5cbac78a4fdb2df73db1f335ee9466b77b829357bbe58593";
 static_assert(sizeof(GENESIS_COINBASE_TX_HEX)/sizeof(*GENESIS_COINBASE_TX_HEX) != 1, "GENESIS_COINBASE_TX_HEX must not be empty.");
 
 /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
